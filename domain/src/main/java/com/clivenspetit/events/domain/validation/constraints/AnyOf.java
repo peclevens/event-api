9116f0d138001d063a65fcb311d999560c444b57
Clivens Petit <clivens.petit@magicsoftbay.com>
2019-10-12 19:51:32 -0400

    feature: allow anyof constraint to group constraint

--- domain/src/main/java/com/clivenspetit/events/domain/validation/constraints/AnyOf.java
+++ domain/src/main/java/com/clivenspetit/events/domain/validation/constraints/AnyOf.java
@@ -12,14 +12,23 @@ import java.lang.annotation.*;
 @Documented
 @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(AnyOf.List.class)
 @Constraint(validatedBy = {AnyOfValidator.class})
 public @interface AnyOf {
 
-    String message() default "{com.clivenspetit.events.domain.validation.constraints.RequireEventLocationOrOnlineUrl.message}";
+    String message() default "{com.clivenspetit.events.domain.validation.constraints.AnyOf.message}";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
 
     String[] fields();
+
+    @Documented
+    @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface List {
+
+        AnyOf[] value();
+    }
 }
