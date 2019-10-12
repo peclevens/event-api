bb1931813f266251ff54707b8c6a101b1eeb11c3
Clivens Petit <clivens.petit@magicsoftbay.com>
2019-10-12 19:49:29 -0400

    feature: add iterable of string validator

---
+++ domain/src/main/java/com/clivenspetit/events/domain/validation/constraints/IterableOfStringPattern.java
@@ -0,0 +1,36 @@
+package com.clivenspetit.events.domain.validation.constraints;
+
+import com.clivenspetit.events.domain.validation.validators.IterableOfStringPatternValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * @author Clivens Petit <clivens.petit@magicsoftbay.com>
+ */
+@Documented
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
+        ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(IterableOfStringPattern.List.class)
+@Constraint(validatedBy = {IterableOfStringPatternValidator.class})
+public @interface IterableOfStringPattern {
+
+    String message() default "{com.clivenspetit.events.domain.validation.constraints.IterableOfStringPattern.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String regexp() default ".*";
+
+    @Documented
+    @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
+            ElementType.PARAMETER, ElementType.TYPE_USE})
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface List {
+
+        IterableOfStringPattern[] value();
+    }
+}
---
+++ domain/src/main/java/com/clivenspetit/events/domain/validation/validators/IterableOfStringPatternValidator.java
@@ -0,0 +1,31 @@
+package com.clivenspetit.events.domain.validation.validators;
+
+import com.clivenspetit.events.domain.validation.constraints.IterableOfStringPattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * @author Clivens Petit <clivens.petit@magicsoftbay.com>
+ */
+public class IterableOfStringPatternValidator
+        implements ConstraintValidator<IterableOfStringPattern, Iterable<String>> {
+
+    private String regexp;
+
+    @Override
+    public void initialize(IterableOfStringPattern annotation) {
+        regexp = annotation.regexp();
+    }
+
+    @Override
+    public boolean isValid(Iterable<String> items, ConstraintValidatorContext context) {
+        if (items == null) return true;
+
+        for (String item : items) {
+            if (!item.matches(this.regexp)) return false;
+        }
+
+        return true;
+    }
+}
