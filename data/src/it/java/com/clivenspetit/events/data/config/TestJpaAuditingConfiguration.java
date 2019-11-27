582177143bd105cbd668fc2440b015fcac943cac
Clivens Petit <clivens.petit@magicsoftbay.com>
2019-11-27 01:48:13 -0400

    refactor: re-implement entity audit aware to query user id at runtime

--- data/src/it/java/com/clivenspetit/events/data/config/TestJpaAuditingConfiguration.java
+++ data/src/it/java/com/clivenspetit/events/data/config/TestJpaAuditingConfiguration.java
@@ -16,12 +16,23 @@
 
 package com.clivenspetit.events.data.config;
 
+import com.clivenspetit.events.data.StubContext;
+import com.clivenspetit.events.data.security.audit.DefaultAuditorAware;
+import com.clivenspetit.events.domain.Context;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
+import javax.persistence.EntityManager;
 
 /**
  * @author Clivens Petit
@@ -30,8 +41,24 @@ import java.util.Optional;
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class TestJpaAuditingConfiguration {
 
+    private static final MutableConfiguration<String, Long> userIdsConfig = new MutableConfiguration<>();
+    private static final CachingProvider cachingProvider = Caching.getCachingProvider();
+
     @Bean
-    public AuditorAware<Long> auditorProvider() {
-        return () -> Optional.of(1L);
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public AuditorAware<Long> auditorProvider(EntityManager entityManager, Context context) {
+        String cacheName = "userIds";
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+        Cache<String, Long> userIdCache = cacheManager.getCache(cacheName);
+        if (userIdCache == null) {
+            userIdCache = cacheManager.createCache(cacheName, userIdsConfig);
+        }
+
+        return new DefaultAuditorAware(entityManager, context, userIdCache);
+    }
+
+    @Bean
+    public Context context() {
+        return new StubContext();
     }
 }
---
+++ data/src/main/java/com/clivenspetit/events/data/security/audit/DefaultAuditorAware.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2019 MAGIC SOFTWARE BAY, SRL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.clivenspetit.events.data.security.audit;
+
+import com.clivenspetit.events.data.user.entity.UserEntity;
+import com.clivenspetit.events.domain.Context;
+import com.clivenspetit.events.domain.user.exception.UserNotFoundException;
+import com.clivenspetit.events.domain.user.login.exception.InvalidLoggedInUserException;
+import org.springframework.data.domain.AuditorAware;
+
+import javax.cache.Cache;
+import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
+import javax.persistence.TypedQuery;
+import java.util.Optional;
+
+/**
+ * @author Clivens Petit
+ */
+public class DefaultAuditorAware implements AuditorAware<Long> {
+
+    private final EntityManager entityManager;
+    private final Context context;
+    private final Cache<String, Long> userIdCache;
+
+    public DefaultAuditorAware(EntityManager entityManager, Context context, Cache<String, Long> userIdCache) {
+        this.entityManager = entityManager;
+        this.context = context;
+        this.userIdCache = userIdCache;
+    }
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        // Get the user id
+        String userId = context.getLoggedUser().orElseThrow(InvalidLoggedInUserException::new).getId();
+
+        // If user id is in cache, return it.
+        if (userIdCache.containsKey(userId)) return Optional.of(userIdCache.get(userId));
+
+        String queryString = "SELECT u FROM UserEntity u WHERE u.userId = :userId";
+
+        FlushModeType flushMode = entityManager.getFlushMode();
+        entityManager.setFlushMode(FlushModeType.COMMIT);
+        TypedQuery<UserEntity> query = entityManager.createQuery(queryString, UserEntity.class);
+        query.setParameter("userId", userId);
+
+        // Find user with id
+        UserEntity userEntity = Optional.of(query.getSingleResult()).orElseThrow(UserNotFoundException::new);
+
+        // Set flush mode back
+        entityManager.setFlushMode(flushMode);
+
+        // Cache user id
+        userIdCache.put(userId, userEntity.getId());
+
+        return Optional.of(userEntity.getId());
+    }
+}
